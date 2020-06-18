3784166ab24188d65cb51443cae304b4bc8d94ad
Daniel <dnelis@tcd.ie>
2020-06-18 23:12:00 +0100

    added loop for getting tweets for #TSLA and #TSLAQ with tweepy

--- code/twitter_api/configs/config.py
+++ code/twitter_api/configs/config.py
@@ -1,4 +1,4 @@
-API key = "zmFM2H50UqDhgH7PHMYBwdseD"
+API_key = "zmFM2H50UqDhgH7PHMYBwdseD"
 API_secret_key = "goZ7oOQhCgmoPKLcRl5mHTe2PSKkNQr0mKadUhi1E2Ex6OkxAu"
-access_token = "984800926983745536-FcEC0tNbKtrvsp4MO3o7jjNQ6QYZ5UI"
+access_token_key = "984800926983745536-FcEC0tNbKtrvsp4MO3o7jjNQ6QYZ5UI"
 access_token_secret = "nPjFSUdEn0Il2ZQV6RtXc43hU3oNRto7ejDKzmkJSWA56"
--- code/twitter_api/tweets.py
+++ code/twitter_api/tweets.py
@@ -1,1 +1,30 @@
-print('hello world')
+from configs.config import API_key, API_secret_key,access_token_key,access_token_secret
+
+import tweepy
+
+auth = tweepy.OAuthHandler(API_key, API_secret_key) #extra argument of callback_url added for a dynamic URL call (changing url only)
+auth.set_access_token(access_token_key, access_token_secret)
+
+
+api = tweepy.API(auth)
+
+'''
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
+for status in tweepy.Cursor(api.user_timeline, id="TeslaPodcast").items(100):
+    process_status(status)
+    '''
+
+print('#TSLA')
+for tweet in tweepy.Cursor(api.search,q="#TSLA",
+                           lang="en",
+                           since="2017-04-03").items(5):
+    print (tweet.created_at, tweet.text)
+
+print('#TSLAQ')
+for tweet in tweepy.Cursor(api.search,q="#TSLAQ",
+                           lang="en",
+                           since="2017-04-03").items(5):
+    print (tweet.created_at, tweet.text)
